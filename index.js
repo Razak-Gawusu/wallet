41c16f5e3365cc5fb763bb5b1737b7a7c5592853
Gawusu Razak <gawusuLarbil@gmail.com>
2023-02-23 15:11:36 +0000

    removed .env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("./startup/config")();
 require("./startup/db")();
 require("./startup/routes")(app);
 
-const PORT = process.env.PORT || 5050;
+const PORT = config.get("PORT") || 5050;
 const server = app.listen(PORT, () =>
   debuglog(`successfully running server on port: ${PORT}`)
 );
