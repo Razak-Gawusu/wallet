890b64310a70e850131554ebf099476689451e64
Gawusu Razak <gawusuLarbil@gmail.com>
2023-02-23 12:23:13 +0000

    nodemailer fixed

--- util/email.util.js
+++ util/email.util.js
@@ -3,15 +3,15 @@ const config = require("config");
 
 const sendEmail = async (options) => {
   const transport = nodemailer.createTransport({
-    host: config("host"),
-    port: config("port"),
+    host: config.get("host"),
+    port: config.get("port"),
     auth: {
-      user: config("username"),
-      pass: config("password"),
+      user: config.get("username"),
+      pass: config.get("password"),
     },
   });
   const mailOptions = {
-    from: config.get("wallet@mail.com"),
+    from: config.get("address"),
     to: options.email,
     subject: options.subject,
     text: options.message,
